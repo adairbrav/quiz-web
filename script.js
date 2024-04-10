f045b489ce2baa3d77b29cb5907c9b038cca51c5
adairbrav <158103007+adairbrav@users.noreply.github.com>
2024-04-10 13:36:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  
+  $("#button1" ).click(function(){
+    $("#A").css("color", "Lightgreen");
+  });
+  $("#button2").click(function(){
+    $("#B").css("color", "Lightgreen");
+  });
+  $("#button3").click(function(){
+    $("#D").css("color", "Lightgreen");
+  });
+  $("#button4").click(function(){
+    $("#G").css("color", "Lightgreen");
+  });
+  $("#button5").click(function(){
+    $("#S").css("color", "Lightgreen");
+  });
 });
